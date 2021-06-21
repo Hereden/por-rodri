c5d982609b482978b0320144bfb58fc0b2b13591
Cesar <cesare_ar@hotmail.com>
2021-06-21 12:30:35 -0500

    agregando para añadir una hora xD

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,25 @@ function calcularHora(){
     var theFirstChild = parentElement.firstChild;
     parentElement.insertBefore(nuevoElemento, theFirstChild);
 }
+
+
+function calcularHora2(){
+    let hora = document.getElementById("hora2").value;
+    let hoy = new Date();
+    var nuevoElemento = document.createElement("h1");
+    var textoElemento;
+    nuevoElemento.className += " bg-warning";
+    let sumarsesion=60;
+    if(hora===""){
+        textoElemento = document.createTextNode("Hora actual: "+ hoy.getHours()+":" +hoy.getMinutes() + "  Hora en 1 hora: "+ (hoy.setMinutes(hoy.getMinutes() + sumarsesion) && hoy.getHours() +":" + hoy.getMinutes()));
+    }else{
+        hoy.setHours(hora.substring(0,2));
+        hoy.setMinutes(hora.substring(3,6));
+        textoElemento = document.createTextNode("Hora actual: "+ hoy.getHours()+":" +hoy.getMinutes() + "  Hora en 1 hora: "+ (hoy.setMinutes(hoy.getMinutes() + sumarsesion) && hoy.getHours() +":" + hoy.getMinutes()));
+    }
+
+    nuevoElemento.appendChild(textoElemento);
+    var parentElement = document.getElementById('resultadoParent2');
+    var theFirstChild = parentElement.firstChild;
+    parentElement.insertBefore(nuevoElemento, theFirstChild);
+}
