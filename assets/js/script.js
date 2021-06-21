3c713ae101bf09a127a95348521c5d297d1e576d
Cesar <cesare_ar@hotmail.com>
2021-06-21 12:00:55 -0500

    Agregando 15 minutos,falta agarrar bien el input}

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function calcularHora(){
+    let hora = document.getElementById("hora").value;
+    let hoy = new Date();
+    var nuevoElemento = document.createElement("h1");
+    var textoElemento;
+    nuevoElemento.className += " bg-warning";
+    let sumarsesion=15;
+    if(hora===""){
+        textoElemento = document.createTextNode("Hora actual: "+ hoy.getHours()+":" +hoy.getMinutes() + "  Hora en 15 min: "+ (hoy.setMinutes(hoy.getMinutes() + sumarsesion) && hoy.getHours() +":" + hoy.getMinutes()));
+    }
+
+    nuevoElemento.appendChild(textoElemento);
+    var parentElement = document.getElementById('resultadoParent');
+    var theFirstChild = parentElement.firstChild;
+    parentElement.insertBefore(nuevoElemento, theFirstChild);
+}
